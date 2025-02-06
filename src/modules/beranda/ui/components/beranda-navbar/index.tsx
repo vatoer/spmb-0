add6e73f9db80b44b3f82d282e0dd95781b9b622
vatoer <vatoer.ckplus@gmail.com>
2025-02-06 18:27:21 +0100

    auth button

--- src/modules/beranda/ui/components/beranda-navbar/index.tsx
+++ src/modules/beranda/ui/components/beranda-navbar/index.tsx
@@ -2,6 +2,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
 import SearchInput from "./search-input";
+import AuthButton from "@/modules/auth/ui/components/auth-button";
 
 export const BerandaNavbar = () => {
   return (
@@ -21,6 +22,11 @@ export const BerandaNavbar = () => {
         {/* Search bar*/}
         <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
           <SearchInput />
+        </div>
+
+        {/* Profile */}
+        <div className="flex-shrink-0 items-center flex gap-4">
+          <AuthButton />
         </div>
       </div>
     </nav>
